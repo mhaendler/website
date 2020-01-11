bb3b39b18f4cdfa1fd264680d0d044f7e516d447
Markus Händler <mhaendler@allbytes.de>
2020-01-11 20:54:19 +0100

    Implementing the Basic Idea of the Website

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<!--
+	Identity by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+	<title>Markus Händler</title>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	<link rel="stylesheet" href="assets/css/main.css"/>
+</head>
+<body class="is-loading">
+<div id="wrapper">
+	<section id="main">
+		<header>
+			<span class="avatar"><img src="images/new_me.jpg" alt="Profile Picture of me"/></span>
+			<h1>Markus Händler</h1>
+            <ul>
+                <li>Fachinformatiker für Anwendungsentwicklung</li>
+                <li>Technischer Assistent für Informatik</li>
+            </ul>
+		</header>
+		<hr/>
+		<footer>
+			<ul class="icons">
+				<li><a href="mailto:info@mhaendler.me" class="fa-envelope"></a></li>
+				<li><a target="_blank" title="Link to Xing Profile" href="https://www.xing.com/profile/Markus_Haendler2" class="fa-xing">Xing</a></li>
+				<li><a target="_blank" title="Link to Linked In Profile" href="https://www.linkedin.com/in/markus-h%C3%A4ndler-942636164/" class="fa-linkedin">LinkedIn</a>
+				</li>
+			</ul>
+		</footer>
+	</section>
+</div>
+<!--[if lte IE 8]>
+<script src="assets/js/respond.min.js"></script><![endif]-->
+<script>
+	if ('addEventListener' in window) {
+		window.addEventListener('load', function () {
+			document.body.className = document.body.className.replace(/\bis-loading\b/, '');
+		});
+		document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
+	}
+</script>
+
+</body>
+</html>
