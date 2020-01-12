cb303de9850a9b42043e58fcafdd0a8325522dcb
Markus Händler <mhaendler@allbytes.de>
2020-01-12 20:25:08 +0100

    Implementing the first over me version Adding github Link

--- index.php
+++ index.php
@@ -27,18 +27,26 @@
 			<ul class="icons">
 				<li><a href="mailto:info@mhaendler.me" class="fa-envelope"></a></li>
 				<li><a target="_blank" title="Link to Xing Profile" href="https://www.xing.com/profile/Markus_Haendler2" class="fa-xing">Xing</a></li>
-				<li><a target="_blank" title="Link to Linked In Profile" href="https://www.linkedin.com/in/markus-h%C3%A4ndler-942636164/" class="fa-linkedin">LinkedIn</a>
-				</li>
+				<li><a target="_blank" title="Link to Linked In Profile" href="https://www.linkedin.com/in/markus-h%C3%A4ndler-942636164/" class="fa-linkedin">LinkedIn</a></li>
+                <li><a target="_blank" title="Link to my Github Profile" class="fa-github" href="https://github.com/mhaendler">Github</a></li>
 			</ul>
 		</div>
 	</section>
     <div class="page-break"></div>
     <section id="about-me">
-
+        <div class="dividing-line"><h2>Über mich</h2></div>
+        <p>Vater, Programmierer und Hundebesitzer. Das sind so die 3 wichtigsten Schlagworte wie man mich beschreiben kann.</p>
+        <p>Vor über 14 Jahren endeckte ich meine Leidenschaft zu programmieren. Von Webseiten, über Crawler / Bots, bis hin zu
+            individuellen Softwarelösungen oder Automatisierungen.
+        </p>
+        <p>In meiner "Freizeit" arbeite ich als Freelancer für Webseiten und Anwendungen und nutze diese Gelegenheiten
+           um stetig weiterzuentwickeln und mich in neue Technologien einzuarbeiten. Damit ich meine Kunden
+            immer besser bedienen und meinen Drang neues zu lernen befrieden kann
+        </p>
     </section>
     <div class="page-break"></div>
     <section id="curriculum-vitae">
-        <div class="dividing-line"><h2>Kurzes Faktenblatt</h2></div>
+        <div class="dividing-line"><h3>Kurzes Faktenblatt</h3></div>
         <div class="curriculum-vitae-headline">Ausbildung</div>
         <div class="curriculum-vitae-entry">
             <div class="time">2009 - 2011</div>
